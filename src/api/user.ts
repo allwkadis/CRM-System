41330d802d41b0effc6d2ec35ea8cf87e606bfc7
allwkadis <kadisbinance@gmail.com>
2025-06-09 15:56:19 +0500

    add register page, login page

---
+++ src/api/user.ts
@@ -0,0 +1,7 @@
+import { API_ROUTES } from '../constants/routes';
+import type { AuthData, UserRegistration } from '../types/api';
+import { baseApiAxios } from './baseApi';
+
+export const userSignIn = (authData: AuthData) => baseApiAxios.post(API_ROUTES.SIGN_IN, authData);
+
+export const userRegister = (userData: UserRegistration) => baseApiAxios.post(API_ROUTES.SIGN_UP, userData)
