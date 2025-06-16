fc6316c8660b06dc83a1d6c589a1eea181a0cc0f
allwkadis <kadisbinance@gmail.com>
2025-06-16 16:02:00 +0500

    fix/ fix api

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,19 +1,51 @@
+import type { Todo, TodoStatusVariant } from '../types/api';
+
 import { API_ROUTES } from '../constants/routes';
-import type { TodoStatusVariant } from '../types/api';
 import { baseApiAxios } from './baseApi';
 
-export const getAllTodos = (filter: TodoStatusVariant) =>
-  baseApiAxios.get(API_ROUTES.TODOS, {
-    params: { filter },
-  });
+export const getAllTodos = (filter: TodoStatusVariant) => {
+  try {
+    const response = baseApiAxios.get(API_ROUTES.TODOS, {
+      params: { filter },
+    });
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
 
-//fdsfdsfdsfdsfdsfds
+export const createTodo = async (title: string) => {
+  try {
+    const response = await baseApiAxios.post<Todo>(API_ROUTES.TODOS, { title, isDone: false });
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
 
-export const createTodo = (title: string) => baseApiAxios.post(API_ROUTES.TODOS, { title, isDone: false });
+export const getTodoById = (id: number) => {
+  try {
+    const response = baseApiAxios.get<Todo>(`${API_ROUTES.TODOS}/${id}`);
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
 
-export const getTodoById = (id: number) => baseApiAxios.get(`${API_ROUTES.TODOS}/${id}`, {});
+export const updateTodo = (id: number, title: string, isDone: boolean) => {
+  try {
+    const response = baseApiAxios.put<Todo>(`${API_ROUTES.TODOS}/${id}`, { title, isDone });
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
 
-export const updateTodo = (id: number, title: string, isDone: boolean) =>
-  baseApiAxios.put(`${API_ROUTES.TODOS}/${id}`, { title, isDone });
-
-export const deleteTodo = (id: number) => baseApiAxios.delete(`${API_ROUTES.TODOS}/${id}`);
+export const deleteTodo = (id: number) => {
+  try {
+    const response = baseApiAxios.delete(`${API_ROUTES.TODOS}/${id}`);
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
