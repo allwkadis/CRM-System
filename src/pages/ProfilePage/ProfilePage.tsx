627a268c0d5d2dc9b9ddfb9ced116df28e746b41
allwkadis <kadisbinance@gmail.com>
2025-06-20 14:43:19 +0500

    fix/ remove changePasswordModal from profilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,18 +1,16 @@
 import { useEffect } from 'react';
-import { Typography, Flex, Card, Button, App, Form, Input, Descriptions, Space, Avatar } from 'antd';
-import { LockOutlined, LogoutOutlined, MailOutlined, PhoneOutlined, UserOutlined } from '@ant-design/icons';
+import { Typography, Flex, Card, Button, App, Descriptions, Space, Avatar } from 'antd';
+import { LogoutOutlined, MailOutlined, PhoneOutlined, UserOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router';
 
 import { useAppDispatch, useAppSelector } from '../../store/store';
 import { getProfileInfoAction } from '../../store/actions/getProfileData';
 import { userSlice } from '../../store/slices/userSlice';
-import { authUserLogout, authUserResetPassword } from '../../api/auth';
+import { authUserLogout } from '../../api/auth';
 import { tokenManager } from '../../utils/TokenManager';
-import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from '../../utils/constants/auth';
 
 export const ProfilePage = () => {
-  const { modal, notification } = App.useApp();
-  const [form] = Form.useForm();
+  const { notification } = App.useApp();
   const { isLoading, profileData } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -35,84 +33,6 @@ export const ProfilePage = () => {
     }
   };
 
-  const changePasswordModal = () => {
-    const resetPasswordModal = modal.confirm({
-      title: 'Изменение пароля',
-      closable: true,
-      footer: null,
-      content: (
-        <Form
-          layout="vertical"
-          form={form}
-          onFinishFailed={() => {
-            notification.error({
-              message: 'Ошибка',
-              description: 'Невалидные данные',
-            });
-          }}
-          onFinish={async () => {
-            const password = form.getFieldValue('password');
-            try {
-              await authUserResetPassword(password);
-              notification.success({
-                message: 'Успешно',
-                description: 'Пароль изменен',
-              });
-              form.resetFields();
-              resetPasswordModal.destroy();
-            } catch {
-              notification.error({
-                message: 'Ошибка',
-                description: 'При изменении пароль произошла ошибка',
-              });
-            }
-          }}
-        >
-          <Form.Item
-            label="Пароль"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Пароль обязательное поле!',
-              },
-              {
-                min: PASSWORD_MIN_LENGTH,
-                message: `Пароль не может быть меньше ${PASSWORD_MIN_LENGTH}`,
-              },
-              {
-                max: PASSWORD_MAX_LENGTH,
-                message: `Пароль не может быть больше ${PASSWORD_MIN_LENGTH}`,
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            label="Повторите пароль"
-            name="repeat-password"
-            rules={[
-              { required: true, message: 'Повторите пароль обязательное поле!' },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(new Error('Пароли не совпадают!'));
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="submit">отправить</Button>
-          </Form.Item>
-        </Form>
-      ),
-    });
-  };
-
   if (isLoading) return <div>loading</div>;
 
   return (
@@ -128,9 +48,6 @@ export const ProfilePage = () => {
           </Space>
         }
         actions={[
-          <Button type="primary" icon={<LockOutlined />} onClick={changePasswordModal}>
-            Изменить пароль
-          </Button>,
           <Button danger icon={<LogoutOutlined />} onClick={onLogoutHanddler}>
             Выйти
           </Button>,
